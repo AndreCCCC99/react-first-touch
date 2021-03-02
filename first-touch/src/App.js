5c9a20a234184ab2b69a2b4fe2687ef442fb0e75
André Cirilo <andre.cirilo@empifarma.pt>
2021-03-02 21:25:46 +0000

    react-router-dom update

--- first-touch/src/App.js
+++ first-touch/src/App.js
@@ -1,48 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import Home from './Views/Home'
-import Profile from './Views/Profile'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Views/Home";
+import Profile from "./Views/Profile";
 
+//hooks só podem ser usados em function components
+//não funcionam em class components
+// os hooks correm ordenadamente
+// tem de estar dentro da função
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+//useState(4) default state 4
+//const [count, setCount] = useState(4) primeiro indice é o valor associado / o segundo indice serve para associar um valor
 
 function App() {
+  const [count, setCount] = useState(4);
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
   return (
     <Router>
       <div>
         <nav>
           <ul>
             <li>
-              <Link exact to="/">App</Link>
-            </li>
-            <li>
-              <Link to="/Home">Home</Link>
+              <Link to="/">Home</Link>
             </li>
             <li>
               <Link to="/Profile">Profile</Link>
             </li>
           </ul>
         </nav>
-
-        <Switch>
-          <Route exact path="/">
-              <App />
-          </Route>
-          <Route path="/Home">
-              <Home />
-          </Route>
-          <Route path="/Profile">
-              <Profile />
-          </Route>
-        </Switch>
       </div>
-    </Router>
 
+      <button onClick={decrement}>-</button>
+      <span>0</span>
+      <button>+</button>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/Profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
